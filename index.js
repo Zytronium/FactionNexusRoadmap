e45064468240f33acfb965c17a12f3d183e0bacc
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2025-02-15 00:53:17 -0600

    Added styling to each type of item and added an item to the roadmap. The animation on the roadmap item widgets is messed up again, but it's hard to notice.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll(".animate-vertically-on-load").forEach(element => {
         element.style.overflow = "hidden";
-        element.style.transition = "height 1.5s ease-out";
+        element.style.transition = "height 2s ease-out";
 
         // Measure the full height including padding but ignoring margins
         let computedStyle = window.getComputedStyle(element);
@@ -23,8 +23,16 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(() => {
             element.style.height = ""; // Reset to natural height
             element.style.overflow = "visible";
-        }, 1500);
+        }, 2000);
 
         element.classList.remove("animate-vertically-on-load");
+    });
+
+    document.querySelectorAll(".item").forEach(element => {
+        let type = element.classList.item(0);
+        let type_text = element.children.item(0).children.item(1);
+
+        type_text.textContent = type;
+
     });
 });
